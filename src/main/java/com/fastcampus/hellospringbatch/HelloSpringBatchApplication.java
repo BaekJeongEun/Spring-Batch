acd4eddd33502f4f5230ff85ee2f32b0dbd8023c
BaekJeongEun <sophia2404@naver.com>
2023-05-10 17:55:06 +0900

    [ADD]: 잡이 종료되면 프로그램 중단하게 만들기

--- src/main/java/com/fastcampus/hellospringbatch/HelloSpringBatchApplication.java
+++ src/main/java/com/fastcampus/hellospringbatch/HelloSpringBatchApplication.java
@@ -1,13 +1,21 @@
 package com.fastcampus.hellospringbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class HelloSpringBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringBatchApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        // Before
+        SpringApplication.run(HelloSpringBatchApplication.class, args);
+        // After
+        System.exit(
+            SpringApplication.exit(
+                SpringApplication.run(HelloSpringBatchApplication.class, args)
+            )
+        );
+    }
 }
