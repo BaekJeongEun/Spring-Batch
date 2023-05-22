c91987d8080aaae97bbb684cced00c26fd60ab45
BaekJeongEun <sophia2404@naver.com>
2023-05-22 21:20:04 +0900

    [ADD]: Job Configuration

---
+++ src/main/java/com/fastcampus/hellospringbatch/config/simple/HelloBatchJobConfiguration.java
@@ -0,0 +1,40 @@
+package com.fastcampus.hellospringbatch.config.simple;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@AllArgsConstructor
+class HelloBatchJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public static final String JOB_NAME = "helloJob";
+    public static final String STEP_1_NAME = "helloStep";
+    public static final String MESSAGE = "Hello, Spring Batch!!";
+
+
+    @Bean
+    public Job helloJob() {
+        return this.jobBuilderFactory.get(JOB_NAME).start(helloStep()).build();
+    }
+
+    @Bean
+    public Step helloStep() {
+        return this.stepBuilderFactory.get(STEP_1_NAME)
+            .tasklet((contribution, chunkContext) -> {
+                log.info("This is HelloStep, {}", MESSAGE);
+                return RepeatStatus.FINISHED;
+            }).build();
+    }
+
+}
